21247008f4f33f489f126a7473b0c9b311381828
Manuel <mjtrujillo92@gmail.com>
2016-02-02 16:45:59 -0500

    My first Prog Pal commit

---
+++ 001_input_to_num/001_input_to_num.c
@@ -0,0 +1,52 @@
+// Manuel J. Trujillo-Silva
+// Roman Numeral to Number
+#include <stdio.h>
+
+
+int main()
+{
+	// Declare variables
+	char rn_chord[6], num_chord[128];
+    int num_out;
+
+	// Compared upper/lowercase RN variables
+	char iv_low[] = "iv", iv_up[] = "IV", vi_low[] = "vi", vi_up[] = "VI", vii_low[] = "vii", vii_up[] = "VII";
+
+	// Ask for input
+	printf("What chord are you currently using?\n");
+	scanf("%s", rn_chord);
+
+
+
+
+	// Numeral to Input
+	if (strcmp(rn_chord,"i") == 0 || strcmp(rn_chord, "I") == 0)
+	{
+        num_out = 1;
+	}
+	else if (strcmp(rn_chord, "ii") == 0 || strcmp(rn_chord, "II") == 0)
+	{
+	num_out = 2;
+	}
+	else if (strcmp(rn_chord, "iii") == 0 || strcmp(rn_chord, "III") == 0)
+	{
+	num_out= 
+	}
+	else if (strcmp(rn_chord, "iv") == 0 || strcmp(rn_chord, "IV") == 0)
+	else if (strcmp(rn_chord, "v") == 0 || strcmp(rn_chord, "V") == 0)
+	
+	
+	
+	
+	
+	else
+	{
+		printf("Fuck nope...\n");
+	}
+
+
+
+
+
+
+}
---
+++ Cir_Prog.c
@@ -0,0 +1,59 @@
+// Manuel J. Trujillo-Silva
+// Circle Progression
+#include <stdio.h>
+int main()
+{
+	//Declare variables
+	int CUR_CHORD_NUM, NEX_CHORD_NUM;
+
+	//Request input
+	printf("What chord are you currently using? (input should be 1 through 7)\n");
+	scanf("%d", &CUR_CHORD_NUM);
+
+	//Progression switch
+
+	switch (CUR_CHORD_NUM)
+	{
+		case 1:
+		{
+		NEX_CHORD_NUM = 4;
+		break;
+		}
+		case 2:
+		{
+		NEX_CHORD_NUM = 5;
+		break;
+		}
+		case 3:
+		{
+		NEX_CHORD_NUM = 6;
+		break;
+		}
+		case 4:
+		{
+		NEX_CHORD_NUM = 7;
+		break;
+		}
+		case 5:
+		{
+		NEX_CHORD_NUM = 1;
+		break;
+		}
+		case 6:
+		{
+		NEX_CHORD_NUM = 2;
+		break;
+		}
+		case 7:
+		{
+		NEX_CHORD_NUM = 3;
+		break;
+		}
+
+	}
+
+	//Output
+	printf("Your next chord should be a %d chord.\n", NEX_CHORD_NUM);
+
+
+}
