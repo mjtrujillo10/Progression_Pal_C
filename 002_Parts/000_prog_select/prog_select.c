862b6b56286f09083fce4007786fefaa21fc539f
Manuel <mjtrujillo92@gmail.com>
2016-02-05 20:11:57 -0500

    Create Input for selecting a progression

---
+++ 002_Parts/000_prog_select/prog_select.c
@@ -0,0 +1,21 @@
+// 
+// Progression Selection
+#include <stdio.h>
+int main()
+{
+	// Declare variables
+	int prog_num;
+
+	// Ask for i/o
+	printf("\nPlease select one of the following progressions:");
+	printf("\n01) Circle Progression");
+	printf("\n02) Reverse Circle Progression");
+	scanf("", &prog_num);
+	// Manipulate variables
+	switch (prog_num)
+		case 1||:
+		{
+		}
+		case 2:
+	 
+}
